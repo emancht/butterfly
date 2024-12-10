535f09181a69654507da6175fcd2de3b106d8623
emancht <emancht@gmail.com>
2024-12-10 11:03:21 +0600

    first commit

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', 
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist', // Directory for production build
+  },
+});
